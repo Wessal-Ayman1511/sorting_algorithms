85a12b27921e03fdeb0dac886d9ec5431cd9aeb3
Wessal Ayman <wessalayman237@gmail.com>
2024-03-05 08:47:35 -0800

    Wessal

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,11 +11,11 @@ void bubble_sort(int *array, size_t size)
 	int temp;
 
 	if (!array || !size)
-	return;
+		return;
 
-	for (i = 0 ; i < size - 1 ; i++)
+	for (i = 0 ; i < size  ; i++)
 	{
-		for (j = 0 ; j < size - i - 1 ; j++)
+		for (j = 0 ; j < size - 1 ; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
