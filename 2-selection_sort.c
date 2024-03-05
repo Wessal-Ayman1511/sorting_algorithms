9159a8c6f4fc5305fae312a677b47e3c088e6b81
Wessal Ayman <wessalayman237@gmail.com>
2024-03-05 11:54:22 -0800

    bubble

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,27 +6,30 @@
 */
 void selection_sort(int *array, size_t size)
 {
-    size_t minIdx, i, j;
-    int tmp;
+	size_t minIdx, i, j;
+	int tmp;
 
-    if(!array || !size || size < 2)
-    return;
+	if(!array || !size || size < 2)
+	return;
 
-    for(i = 0; i < size - 1; i++)
-    {
-        minIdx = i;
-        for(j = i + 1 ; j < size ; j++)
-        {
-            if(array[j] < array[minIdx])
-            {
-                minIdx = j;
-            }
-        }
-        tmp = array[i];
-        array[i] = array[minIdx];
-        array[minIdx] = tmp;
-        print_array(array, size); 
+	for(i = 0; i < size - 1; i++)
+	{
+		minIdx = i;
+		for(j = i + 1 ; j < size ; j++)
+		{
+			if(array[j] < array[minIdx])
+			{
+				minIdx = j;
+			}
+		}
+		if (minIdx != i)
+		{
+			tmp = array[i];
+			array[i] = array[minIdx];
+			array[minIdx] = tmp;
+			print_array(array, size); 
+		}
 
 
-    }
+	}
 }
